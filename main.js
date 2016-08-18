48d86e9ad75b19739b1e351814704969a9521960
Anton Arboleda <anton.arboleda@gmail.com>
2016-08-17 19:30:44 -0700

    Refactored JS files

---
+++ main.js
@@ -0,0 +1,9 @@
+var socket = io();
+$('form').submit(function() {
+	socket.emit('chat message', $('#m').val());
+	$('#m').val('');
+	return false;
+});
+socket.on('chat message', function(message) {
+	$('#messages').append($('<li>').text(message));
+});
