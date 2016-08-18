0124a857589f1ddfe38ae7cfcda20b676a0ff91e
Michael Mitrakos <mike.mitrakos@gmail.com>
2016-08-17 22:02:24 -0700

    Delete duplicate file

--- js/main.js
+++
@@ -1,9 +0,0 @@
-var socket = io();
-$('form').submit(function() {
-	socket.emit('chat message', $('#m').val());
-	$('#m').val('');
-	return false;
-});
-socket.on('chat message', function(message) {
-	$('#messages').append($('<li>').text(message));
-});
