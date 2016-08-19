0a7d444567574fd8e31e42fd740b1731729e86b2
Michael Mitrakos <mike.mitrakos@gmail.com>
2016-08-19 11:07:13 -0700

    Pushing video capability, leading to GIF support on send message

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 var socket = io();
-var nameVariable = "";
-var username;
+var currentUser = "";
+var numUsers = 1;
 
 // function setUsername () {
 // 	username = $('#usernameInput').val();
@@ -37,6 +37,101 @@ socket.on('chat message', function(message) {
 	$('#messages').prepend($('<li>').text(nameVariable + ": " + message));
 	// console.dir(data); -- Commented out until further need
 });
+
+function init() {
+
+	var main = document.querySelector('main');
+	var mosaicContainer = document.getElementById('mosaic');
+	var videoWidth= 0, videoHeight = 0;
+	var videoElement;
+	var shooter;
+	var imagesPerRow = 5;
+	var maxImages = 20;
+
+	window.addEventListener('resize', onResize);
+
+	GumHelper.startVideoStreaming(function(error, stream, videoEl, width, height) {
+		if(error) {
+			alert('Cannot open the camera. Sad times: ' + error.message);
+			return;
+		}
+
+		videoElement = videoEl;
+		videoElement.width = width / 4;
+		videoElement.height = height / 4;
+		videoWidth = width;
+		videoHeight = height;
+
+		main.appendChild(videoElement);
+
+		shooter = new VideoShooter(videoElement);
+
+		onResize();
+
+		startCapturing();
+	});
+
+	function startCapturing() {
+
+		shooter.getShot(onFrameCaptured, 10, 0.2, function onProgress(progress) {
+      console.log("getShot called")
+			//Animate a progress bar or similar using the `progress` value
+		});
+
+	}
+
+	function onFrameCaptured(pictureData) {
+		var img = document.createElement('img');
+		img.src = pictureData;
+    console.log("Frame captured");
+
+		var imageSize = getImageSize();
+
+		img.style.width = imageSize[0] + 'px';
+		img.style.height = imageSize[1] + 'px';
+
+		mosaicContainer.insertBefore(img, mosaicContainer.firstChild);
+
+		if(mosaicContainer.childElementCount > maxImages) {
+			mosaicContainer.removeChild(mosaicContainer.lastChild);
+		}
+
+		setTimeout(startCapturing, 10);
+	}
+
+	function getImageSize() {
+		var windowWidth = window.innerWidth;
+		var imageWidth = Math.round(windowWidth / imagesPerRow);
+		var imageHeight = (imageWidth / videoWidth) * videoHeight;
+
+		return [ imageWidth, imageHeight ];
+	}
+
+	function onResize(e) {
+
+		// Don't do anything until we have a video element from which to derive sizes
+		if(!videoElement) {
+			return;
+		}
+
+		var imageSize = getImageSize();
+		var imageWidth = imageSize[0] + 'px';
+		var imageHeight = imageSize[1] + 'px';
+
+		for(var i = 0; i < mosaicContainer.childElementCount; i++) {
+			var img = mosaicContainer.children[i];
+			img.style.width = imageWidth;
+			img.style.height = imageHeight;
+		}
+
+		videoElement.style.width = imageWidth;
+		videoElement.style.height = imageHeight;
+
+	}
+
+}
+
+window.addEventListener('DOMContentLoaded', init);
 
 //create a jquery funciton
 //so that name input variable is assigned to each text
