a3cabd055010a937ff3e70d76f8ec92b3ea77e0c
Anton Arboleda <anton.arboleda@gmail.com>
2016-08-18 20:29:22 -0700

    deleted extra main.js folder

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,5 @@
 var socket = io();
+var nameVariable = "";
 $('form').submit(function() {
 	socket.emit('chat message', $('#m').val());
 	$('#m').val('');
@@ -8,3 +9,18 @@ socket.on('chat message', function(message) {
 	$('#messages').prepend($('<li>').text(message));
 	console.dir(data);
 });
+
+$('#nameInput').submit(function () {
+    nameVariable = $('#nameSubmit').val();
+    $('#nameSubmit').val('');
+});
+
+//create a jquery funciton
+//so that name input variable is assigned to each text
+
+//should be submitted
+// //$(document).ready(function(){
+
+//    $(selector).action()
+
+// });
