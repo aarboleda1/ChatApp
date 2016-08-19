2e7c03374c8cfd1c2689c5a6e23dd799c1f320c2
Michael Mitrakos <mike.mitrakos@gmail.com>
2016-08-18 20:48:50 -0700

    Added naming functionality

--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,26 @@
 var socket = io();
 var nameVariable = "";
-$('form').submit(function() {
+console.log(nameVariable);
+
+// Save user name
+$('.nameInput').submit(function () {
+    nameVariable = $('#nameSubmit').val();
+		console.log(nameVariable);
+    $('#nameSubmit').val('');
+		return false;
+});
+
+// Post message to chatroom
+$('.sendchat').submit(function() {
 	socket.emit('chat message', $('#m').val());
 	$('#m').val('');
 	return false;
 });
-socket.on('chat message', function(message) {
-	$('#messages').prepend($('<li>').text(message));
-	console.dir(data);
-});
 
-$('#nameInput').submit(function () {
-    nameVariable = $('#nameSubmit').val();
-    $('#nameSubmit').val('');
+// Listen for chat message to be submitted
+socket.on('chat message', function(message) {
+	$('#messages').prepend($('<li>').text(nameVariable + ": " + message));
+	// console.dir(data); -- Commented out until further need
 });
 
 //create a jquery funciton
